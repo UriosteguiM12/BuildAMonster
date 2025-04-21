8c9d77b1cf9a7e293923b0597da039688830d75a
UriosteguiM12 <marinauriostegui88@gmail.com>
2025-04-20 21:26:54 -0700

    first version of BuildAMonster

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -6,6 +6,11 @@ class Monster extends Phaser.Scene {
         //Create constants for the monster location
         this.bodyX = 300;
         this.bodyY = 350;
+        this.sKey = null;
+        this.fKey = null;
+        this.aKey = null;
+        this.dKey = null;
+
         
     }
 
@@ -31,15 +36,53 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.rightArm = this.add.sprite(this.bodyX+70, this.bodyY+70, "monsterParts", "arm_greenA.png");
+        my.sprite.leftArm = this.add.sprite(this.bodyX-70, this.bodyY+70, "monsterParts", "arm_greenA.png");
+        my.sprite.leftArm.flipX=true;
+        my.sprite.rightLeg = this.add.sprite(this.bodyX+30, this.bodyY+150, "monsterParts", "leg_redA.png");
+        my.sprite.rightLeg.scale=0.75; 
+        my.sprite.leftLeg = this.add.sprite(this.bodyX-30, this.bodyY+150, "monsterParts", "leg_redA.png");
+        my.sprite.leftLeg.flipX=true;
+        my.sprite.leftLeg.scale=0.75; 
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_yellowF.png");
+        my.sprite.leftEye = this.add.sprite(this.bodyX-30, this.bodyY-30, "monsterParts", "detail_white_ear_round.png");
+        my.sprite.rightEye = this.add.sprite(this.bodyX+30, this.bodyY-30, "monsterParts", "eye_dead.png");
+        my.sprite.mouthSmile = this.add.sprite(this.bodyX, this.bodyY+20, "monsterParts", "mouthI.png");
+        my.sprite.mouthFang = this.add.sprite(this.bodyX, this.bodyY+20, "monsterParts", "mouthJ.png");
+        my.sprite.mouthFang.visible=false;
+        my.sprite.rightAntennae = this.add.sprite(this.bodyX+50, this.bodyY-100, "monsterParts", "detail_blue_antenna_small.png");
+        my.sprite.leftAntennae = this.add.sprite(this.bodyX-50, this.bodyY-100, "monsterParts", "detail_blue_antenna_small.png");
+        my.sprite.leftAntennae.flipX=true;
 
-        
+        this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
 
-       
+        if (Phaser.Input.Keyboard.JustDown(this.sKey)){
+            my.sprite.mouthSmile.visible = true
+            my.sprite.mouthFang.visible = false;
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.fKey)){
+            my.sprite.mouthSmile.visible = false
+            my.sprite.mouthFang.visible = true;
+        }
+
+        for (let bodyPart in my.sprite) {
+            if (this.aKey.isDown) {
+                my.sprite[bodyPart].x -=5;
+            }
+            if (this.dKey.isDown) {
+                my.sprite[bodyPart].x +=5;
+            }
+        }
+
     }
 
 }
